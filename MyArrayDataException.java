7fb8eac721966a9be2adf12e3e089080259cccd6
Andrew-Fisenko <88586401+Andrew-Fisenko@users.noreply.github.com>
2021-09-06 18:08:01 +0300

    Add files via upload

---
+++ MyArrayDataException.java
@@ -0,0 +1,9 @@
+package com.company.JavaCore.Lesson_09;
+
+public class MyArrayDataException extends Exception {
+
+    public MyArrayDataException(String message) {
+        super (message);
+    }
+
+}
---
+++ MyArraySizeException.java
@@ -0,0 +1,9 @@
+package com.company.JavaCore.Lesson_09;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+
+}
